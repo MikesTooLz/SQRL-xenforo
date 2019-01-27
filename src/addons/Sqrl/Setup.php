ad55460e28728d49acb018a166f7d830efcd124e
Rasmus Vind <me@rasmusa.net>
2019-01-27 22:03:42 +0100

    Clean up setup script.

--- src/addons/Sqrl/Setup.php
+++ src/addons/Sqrl/Setup.php
@@ -8,7 +8,6 @@ class Setup extends AbstractSetup
 {
     public function install(array $stepParams = [])
     {
-        // TODO: Implement install() method.
         $db = $this->db();
         $db->query("REPLACE INTO `xf_connected_account_provider` (`provider_id`, `provider_class`, `display_order`, `options`)
         VALUES
@@ -17,11 +16,11 @@ class Setup extends AbstractSetup
 
     public function upgrade(array $stepParams = [])
     {
-        // TODO: Implement upgrade() method.
     }
 
     public function uninstall(array $stepParams = [])
     {
-        // TODO: Implement uninstall() method.
+        $db = $this->db();
+        $db->query("DELETE FROM `xf_connected_account_provider` WHERE `provider_id` = 'sqrl'");
     }
 }
